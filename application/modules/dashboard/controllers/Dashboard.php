5a64f72b4b43df093f649ef4b8936ca7fc98eedb
rickykusriana <rickykusriana@gmail.com>
2017-02-08 20:27:10 +0700

    First Commit

---
+++ application/modules/dashboard/controllers/Dashboard.php
@@ -0,0 +1,34 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends MX_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		if (empty($this->session->userdata('userData'))) {
+			redirect(site_url());
+			exit();
+		}
+	}
+
+	public function index()
+	{
+		$data = $this->attribute();
+		$data['content']	= 'content';
+		$this->load->view('templates/index', $data);
+	}
+
+	public function attribute()
+	{
+		$data['userData']	= $this->session->userdata('userData');
+		$data['attr']		= array(
+								'nav_dashboard' => true,
+								'breadcumb'		=> 'Dashboard');
+		return $data;
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/Dashboard.php */
